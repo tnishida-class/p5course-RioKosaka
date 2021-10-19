4af2897287714ca27800ec16bf024721cac8c738
RioKosaka <91932318+RioKosaka@users.noreply.github.com>
2021-10-19 12:21:53 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,6 +5,17 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      fill((i+j) % 2 == 0 ? 255 :77);
+
+    console.log(i, j);
+    rect(i * 20, j * 20, 20, 20);
+
+
+    if((i+j) % 2 == 1 && j<=2){fill(255, 0, 0);}
+    else  if((i+j) % 2 == 1 && j>=5){fill(0);}
+    ellipse(i*20+10,j*20+10,18)
+
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
