8011a39d0d4ff38ee0c13099fbaa60f819c50556
RioKosaka <91932318+RioKosaka@users.noreply.github.com>
2021-10-19 12:21:35 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,11 @@
 // 小手調べ
 function setup(){
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+  let x= i*5 +5;
+  if(i>=5){stroke(255, 0, 0);}
+  else  if(i<5){stroke(0, 0, 255);}
+  ellipse(50,50, x);
+
+}
 }
