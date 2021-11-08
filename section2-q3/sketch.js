8d6ead3efc5682879a2b04653c7308b4d43c921c
RioKosaka <91932318+RioKosaka@users.noreply.github.com>
2021-11-08 13:37:13 +0900

    Changed section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -14,18 +14,71 @@ function setup() {
   const cy = height / 2;
   const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
 
+
+
   drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
+
+  drawArcs1(green, red, maxR * 0.8);
+//
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawArcs2(cream, black, maxR * 0.75);
+  drawArcs3(green, red, maxR * 0.5);
+  drawArcs4(cream, black, maxR * 0.45);
+  drawCircle(green,maxR* 0.1);
   drawCircle(red, maxR * 0.05);
+
 }
 
 function drawCircle(c, r){
+  const cx = width/2;//中心は（cx,cy)
+  const cy = height/2;
+
   fill(c);
   ellipse(cx, cy, r, r);
 }
 
-function drawArcs(c1, c2, r) {
+function drawArcs1(c1, c2, r) {
+  const cx = width/2;//中心は（cx,cy)
+  const cy = height/2;
+
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+//ここから変更
+
+function drawArcs2(c1, c2, r) {
+    const cx = width/2;//中心は（cx,cy)
+    const cy = height/2;
+
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+function drawArcs3(c1, c2, r) {
+    const cx = width/2;//中心は（cx,cy)
+    const cy = height/2;
+
+  for (let i = 0; i < 20; i++) {
+    let start = TWO_PI / 20 * i;
+    let stop = TWO_PI / 20 * (i + 1);
+    fill(i % 2 == 0 ? c1 : c2);
+    arc(cx, cy, r, r, start, stop, PIE);
+  }
+}
+
+function drawArcs4(c1, c2, r) {
+    const cx = width/2;//中心は（cx,cy)
+    const cy = height/2;
+
   for (let i = 0; i < 20; i++) {
     let start = TWO_PI / 20 * i;
     let stop = TWO_PI / 20 * (i + 1);
