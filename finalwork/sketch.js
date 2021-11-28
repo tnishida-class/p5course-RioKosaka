756bc9345b9d4da88c3b15de5919a4b63bddfa31
RioKosaka <91932318+RioKosaka@users.noreply.github.com>
2021-11-28 15:01:00 +0900

    Changed finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,43 @@
 // 最終課題を制作しよう
 
-function setup(){
+
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255,235,205);
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  background(255,235,205);
+  if(mouseIsPressed){
+  push();
+
+  translate(mouseX, mouseY);
+  rotate(frameCount / 5);
+  drawParasol(6, 40);
+  pop();
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  else{
+    push();
+  translate(300, 300);
+  drawParasol(6, 40);
+  pop();
 }
+}
+
+function drawParasol(n, r){
+
+ for(i = 0; i < TWO_PI; i += 0.01){
+
+    if(sin(i * n) >= 0.3){
+     stroke(color(255,200,0));
+    }
+    else{
+      stroke(color(255,100,0));
+    }
+    line(r * cos(i), r * sin(i), 0, 0);
+    stroke(color(255,100,0));
+    }
+ ellipse(0, 0, 8);
+ }
